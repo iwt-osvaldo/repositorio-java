c42b3c29d07aa84a07316d0cb6e04a90e57e0163
iwt <iwtraining.java@gmail.com>
2021-12-16 20:57:53 -0300

    deletando o exercicios main

--- exercicios-main/aulas-java-agostonovembro/src/aula1/resolucaoExercicios/FraseNaTela.java
+++
@@ -1,8 +0,0 @@
-package aula1.resolucaoExercicios;
-
-public class FraseNaTela {
-
-	public static void main(String[] args) {
-		System.out.println("Seja bem vindo ao curso de lógica de programação Java");
-	}
-}
--- exercicios-main/aulas-java-agostonovembro/src/aula3/exercicios/Aluno.java
+++
@@ -1,9 +0,0 @@
-package aula3.exercicios;
-
-public class Aluno {
-
-	int id;
-	String nome;
-	String curso;
-	
-}
